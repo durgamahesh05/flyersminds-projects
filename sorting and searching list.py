e6d01b30a75dacd2af6a1af28aeea397e7d66a0f
Durga Mahesh Uppu <163436732+durgamahesh05@users.noreply.github.com>
2026-03-23 17:35:03 +0530

    Add files via upload

---
+++ sorting and searching list.py
@@ -0,0 +1,28 @@
+arr = [5, 2, 9, 1, 7]
+
+arr.sort()  
+print("Sorted:", arr)
+
+arr.sort(reverse=True)
+print("Descending:", arr)
+
+def linear_search(arr, key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            return i
+    return -1
+
+def binary_search(arr, key):
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
